49e93e11508f85c188894c048d50523eaa384ed9
Cesar Rodriguez Moreno <idaho@solera.local>
2021-02-20 18:20:52 +0100

    Added size tween

--- Demo.gd
+++ Demo.gd
@@ -8,9 +8,11 @@ extends Node2D
 onready var minx = 128
 onready var maxx = 960 - 128
 onready var miny = 128
-onready var maxy = 540 - 128
+onready var maxy = 540 -128
 onready var halftimex = 1.0
-onready var halftimey = 5.0
+onready var halftimey = 0.3
+onready var mins = Vector2(0.2, 0.2)
+onready var maxs = Vector2(0.8, 0.8)
 
 
 # Called when the node enters the scene tree for the first time.
@@ -21,12 +23,20 @@ func _ready():
 	tweenx.repeat = true
 	tweenx.start()
 	tweenx.seek(halftimex/2.0)
+
 	var tweeny = get_node("TweenY")
 	tweeny.interpolate_property($ShinyBall,"position:y", miny, maxy, halftimey, Tween.TRANS_SINE, Tween.EASE_IN_OUT, 0)
 	tweeny.interpolate_property($ShinyBall,"position:y", maxy, miny, halftimey, Tween.TRANS_SINE, Tween.EASE_IN_OUT, halftimey)
 	tweeny.repeat = true
 	#tweeny.seek(halftimey/2)
 	tweeny.start()
+
+	var tweens = get_node("TweenS")
+	tweens.interpolate_property($ShinyBall,"scale", maxs, mins, halftimex, Tween.TRANS_SINE, Tween.EASE_IN_OUT, 0)
+	tweens.interpolate_property($ShinyBall,"scale", mins, maxs, halftimex, Tween.TRANS_SINE, Tween.EASE_IN_OUT, halftimex)
+	tweens.repeat = true	
+	tweens.start()
+	#tweens.seek(halftimex/2.0)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
